7c42cc8938a88313b48ca1a6e235d166cce443ef
achroobatta <achroo.batta@gmail.com>
2021-12-23 23:47:43 +0530

    Key pair

---
+++ backend-s3.tf
@@ -0,0 +1,32 @@
+resource "aws_s3_bucket" "bkt" {
+  bucket = var.BUCKET_NAME
+  acl    = "private"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+  force_destroy = true
+}
+
+resource "aws_s3_bucket_object" "s3_folder" {
+  bucket = aws_s3_bucket.bkt.id
+  acl    = "private"
+  key    = var.FOLDER_NAME1
+}
+
+terraform {
+  backend "s3" {
+    bucket = "achroo"
+    key    = "terraform/backend"
+    region = "ap-south-1"
+  }
+}
+
+resource "aws_s3_bucket_object" "s3_key" {
+  for_each = fileset("vprofilekey", "vprofilekey.pub")
+  bucket   = aws_s3_bucket.bkt.id
+  acl      = "private"
+  key      = var.FOLDER_NAME2
+  source = "${self.bucket}/${self.key}/${each.value}"
+}
---
+++ vars.tf
@@ -0,0 +1,102 @@
+variable "REGION" {
+  default = "ap-south-1"
+}
+
+variable "BUCKET_NAME" {
+  default = "achroo"
+}
+
+variable "FOLDER_NAME1" {
+  default = "terraform"
+}
+
+variable "FOLDER_NAME2" {
+  default = "sshkey"
+}
+
+variable "AMIs" {
+  type = map(any)
+  default = {
+    ap-south-1 = "ami-0851b76e8b1bce90b"
+  }
+}
+
+variable "PRIVATE_KEY_PATH" {
+  default = "vprofilekey"
+}
+
+variable "PUBLIC_KEY_PATH" {
+  default = "vprofilekey.pub"
+}
+
+variable "USERNAME" {
+  default = "ubuntu"
+}
+
+variable "RMQUSER" {
+  default = "rabbit"
+}
+
+variable "RMQPASS" {
+  default = "Gr33n@pple123456"
+}
+
+variable "DBUSER" {
+  default = "admin"
+}
+
+variable "DBPASS" {
+  default = "admin123"
+}
+
+variable "DBNAME" {
+  default = "accounts"
+}
+
+variable "INSTANCE_COUNT" {
+  default = "1"
+}
+
+variable "VPC_NAME" {
+  default = "vprofile-VPC"
+}
+
+variable "ZONE_A" {
+  default = "ap-south-1a"
+}
+
+variable "ZONE_B" {
+  default = "ap-south-1b"
+}
+
+variable "ZONE_C" {
+  default = "ap-south-1c"
+}
+
+variable "VPC_CIDR" {
+  default = "172.21.0.0/16"
+}
+
+variable "PUB_SUBA_CIDR" {
+  default = "172.21.1.0/24"
+}
+
+variable "PUB_SUBB_CIDR" {
+  default = "172.21.2.0/24"
+}
+
+variable "PUB_SUBC_CIDR" {
+  default = "172.21.3.0/24"
+}
+
+variable "PRIV_SUBA_CIDR" {
+  default = "172.21.4.0/24"
+}
+
+variable "PRIV_SUBB_CIDR" {
+  default = "172.21.5.0/24"
+}
+
+variable "PRIV_SUBC_CIDR" {
+  default = "172.21.6.0/24"
+}
