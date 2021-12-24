e9734984ad0a905842e7a4000acaa1c3f2479960
achroobatta <achroo.batta@gmail.com>
2021-12-24 19:24:56 +0530

    Security Group

--- backend-s3.tf
+++ backend-s3.tf
@@ -31,8 +31,20 @@ resource "aws_s3_bucket_object" "s3_key1" {
 
 terraform {
   backend "s3" {
-    bucket = "achroo"
-    key    = "terraform/backend"
-    region = "ap-south-1"
+    bucket         = "achroo"
+    key            = "terraform/backend"
+    region         = "ap-south-1"
+    dynamodb_table = "state-locking"
   }
 }
+
+resource "aws_dynamodb_table" "state-locking" {
+  name         = "state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
